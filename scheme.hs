7e9d1fcfc51436f01d98a09d05992d538cc5fdde
yusuke <yuusuke.sugimoto@nts.ricoh.co.jp>
2012-07-04 09:48:51 +0900

    initial commit

---
+++ scheme.hs
@@ -0,0 +1,21 @@
+module Main where
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse symbol "lisp" input of
+  Left err  -> "No match: " ++ show err
+  Right val -> "Found value"
+
+
+
+main :: IO ()
+main = do
+  args <- getArgs
+  putStrLn (readExpr(args !! 0))
+
+
